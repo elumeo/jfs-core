8f5ed8c9f889ce999a8906da1139d278f3e00fdd
pascalBrauns <pascal.brauns@juwelo.de>
2020-07-28 12:54:53 +0200

    TECH-428: Added export for history

--- build/Store/index.d.ts
+++ build/Store/index.d.ts
@@ -1,3 +1,4 @@
+export declare const history: import("history").History<{}>;
 declare const _default: (rootEpic: any, rootReducer: any) => import("redux").Store<any, import("redux").AnyAction> & {
     dispatch: {};
 };
--- build/Store/index.js
+++ build/Store/index.js
@@ -3,7 +3,7 @@ import { createEpicMiddleware } from 'redux-observable';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { routerMiddleware } from 'connected-react-router';
 import { createHashHistory } from 'history';
-const history = createHashHistory();
+export const history = createHashHistory();
 export default (rootEpic, rootReducer) => {
     const epicMiddleware = createEpicMiddleware();
     const store = composeWithDevTools(applyMiddleware(epicMiddleware, routerMiddleware(history)))(createStore)(rootReducer);
