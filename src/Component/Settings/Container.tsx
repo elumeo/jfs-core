9a790eae034da7936a485cff8cf1e849f5cee096
pascalBrauns <pascal.brauns@juwelo.de>
2021-04-16 17:41:55 +0200

    TECH-483: Restructured selectors

--- src/Component/Settings/Container.tsx
+++ src/Component/Settings/Container.tsx
@@ -1,96 +1,46 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { useHistory, withRouter } from 'react-router-dom';
-import { History } from 'history';
-import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardActions from '@material-ui/core/CardActions';
-import SelectField from '@material-ui/core/Select';
-
-// import { changeLanguageAction } from 'Store/Action/LanguageAction';
-// import Global from 'Store/Reducer/Global'
+import { useHistory } from 'react-router-dom';
 import Cookie from 'js-cookie';
-
-import './SettingsContainer.scss';
-import { compose } from 'redux';
 import { Language } from 'Types/Language';
-import { Grid, MenuItem } from '@material-ui/core';
+import * as MUI from '@material-ui/core';
 import useActions from 'Store/useActions';
 import { useIntl } from 'react-intl';
 import { useSelector } from 'Types/Redux';
-import { getLanguageStateSelector } from 'Store/Selector/Core/Language';
 
-// ToDo: Duplikat entfernen => Siehe LanguageSettings.tsx
-const LANGUAGES = [
-  {label: 'Deutsch', value: 'de'},
-  {label: 'English', value: 'en'},
-  {label: 'Italiano', value: 'it'}
-];
-
-// interface ISettingsContainerProps {
-//   language?: string;
-//   changeLanguageAction?: typeof changeLanguageAction;
-//   history?: History;
-// }
-
-const  SettingsContainer = () => {
-    const {changeLanguageAction} = useActions();
-    const {back: goBack} = useHistory();
-    const {formatMessage} = useIntl();
-    const language = useSelector(getLanguageStateSelector);
-    // const {
-    //   props: { history: {goBack}, language, changeLanguageAction }
-    // }
-    const menuItems = LANGUAGES.map(lang => <MenuItem key={`settings-menu-item--${lang.value}`} value={lang.value  as Language} >{lang.label}</MenuItem> )
+const Container: React.FC = () => {
+    const { changeLanguageAction } = useActions();
+    const { back: goBack } = useHistory();
+    const { formatMessage } = useIntl();
+    const language = useSelector(state => state.Core.Language.language);
+  
     return (
-      // <Grid container>
-      //   <Grid xs={12} item>
-          <Card style={{width: 330, margin: 'auto'}}>
-                <>
-                  <CardHeader title={formatMessage({id: 'settings.title'})}/>
-                  <SelectField
-                    id='language'
-                    label={formatMessage({id: 'settings.language'})}
-                    // className='md-cell md-cell--12'
-                    value={language}
-                    // itemLabel='label'
-                    // itemValue='value'
-                    onChange={(e) => {
-                      Cookie.set('lang', e.target.value);
-                      changeLanguageAction(e.target.value as Language);
-                    }}>
-                      {menuItems}
-                    </SelectField>
-                  <CardActions className='md-dialog-footer'>
-                    <Button  onClick={goBack}>
-                      {formatMessage({id: 'app.settings.done'})}
-                    </Button>
-                  </CardActions>
-                </>
-          </Card>
-      //   </Grid>
-      // </Grid>
+      <MUI.Card style={{width: 330, margin: 'auto'}}>
+        <MUI.CardHeader title={formatMessage({id: 'settings.title'})}/>
+        <MUI.Select
+          id='language'
+          label={formatMessage({id: 'settings.language'})}
+          value={language}
+          onChange={(e) => {
+            Cookie.set('lang', e.target.value);
+            changeLanguageAction(e.target.value as Language);
+          }}>
+            <MUI.MenuItem value='de'>
+              Deutsch
+            </MUI.MenuItem>
+            <MUI.MenuItem value='en'>
+              English
+            </MUI.MenuItem>
+            <MUI.MenuItem value='it'>
+              Italiano
+            </MUI.MenuItem>
+          </MUI.Select>
+        <MUI.CardActions className='md-dialog-footer'>
+          <MUI.Button  onClick={goBack}>
+            {formatMessage({id: 'app.settings.done'})}
+          </MUI.Button>
+        </MUI.CardActions>
+      </MUI.Card>
     );
-
 }
 
-// const mapStateToProps = (
-//   state: Global.State,
-//   ownProps: ISettingsContainerProps
-// ): ISettingsContainerProps => ({
-//   language: (
-//     state.Core.Language.language
-//       ? state.Core.Language.language
-//       : state.Core.Configuration.config.Language
-//   ),
-//   ...ownProps
-// });
-
-// const enhance = compose(
-//   connect(mapStateToProps, {changeLanguageAction}),
-//   withRouter
-// );
-
-// noinspection JSUnusedGlobalSymbols
-export default SettingsContainer
+export default Container;
