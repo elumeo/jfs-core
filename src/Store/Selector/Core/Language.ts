5d4112ed77bbdaa367078616c94f9e684a1e90ce
pascalBrauns <pascal.brauns@juwelo.de>
2021-04-22 11:56:06 +0200

    TECH-483: Added selectors for language reducer

---
+++ src/Store/Selector/Core/Language.ts
@@ -0,0 +1,12 @@
+import { State } from 'Store/Reducer/Global';
+import { createSelector } from 'reselect';
+
+export const translationLanguage = createSelector(
+  (state: State) => state.Core.Language,
+  state => state.language
+);
+
+export const translations = createSelector(
+  (state: State) => state.Core.Language,
+  state => state.messages
+);
--- src/Store/Selector/Core/index.ts
+++ src/Store/Selector/Core/index.ts
@@ -1,2 +1,3 @@
 export * from './App';
+export * from './Language';
 export * from './WebSocket';
