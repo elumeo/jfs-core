b85e5a658e17874c2e10da1278ad914561aa1d4e
pengbang <yeongi41@googlemail.com>
2021-05-14 12:15:26 +0200

    TECH-483: rebuilt project

--- build/Component/Language/useLanguage.d.ts
+++ build/Component/Language/useLanguage.d.ts
@@ -1,6 +1,6 @@
 import { Language } from "../../Types/Language";
 declare const useLanguage: () => {
     value: Language;
-    onChange: (next: Language) => void;
+    onChange: (next: Language) => import("typesafe-actions").PayloadAction<"language/CHANGE", Language>;
 };
 export default useLanguage;
