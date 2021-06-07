68ebbd3f43922a169fa531ddf2fb6022eb48bc33
pengbang <yeongi41@googlemail.com>
2021-06-07 13:16:58 +0200

    TECH-483: added showcase examples

--- showcase/src/Component/PaperBox.tsx
+++ showcase/src/Component/PaperBox.tsx
@@ -1,19 +1,31 @@
 import React from 'react';
 import { useSelector } from '@elumeo/jfs-core/build/Types/Redux';
-import { Box, Paper,CardHeader, CardContent, Typography } from '@material-ui/core';
-import { withStyles, Theme, WithStyles} from '@material-ui/core/styles';
+import { Box, Paper, CardHeader, CardContent, Typography, Grid, List, ListItem, ListItemIcon, ListItemText, } from '@material-ui/core';
+import { withStyles, Theme, WithStyles } from '@material-ui/core/styles';
 import { useIntl } from 'react-intl';
+import FolderIcon from '@material-ui/icons/Folder';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import ExpandLess from '@material-ui/icons/ExpandLess';
 import { useTheme } from '@material-ui/core/styles';
 import PriceInput from '@elumeo/jfs-core/build/Component/PriceInput'
+function generate(element: React.ReactElement) {
+  return [0, 1, 2].map((value) =>
+    React.cloneElement(element, {
+      key: value,
+    }),
+  );
+}
 type Props = {
 } & WithStyles
 const style = (theme: Theme) => ({})
-const PaperBox :React.FC<Props> = ({
-    classes
-    }) => {
-  const { formatMessage } = useIntl(); 
+const PaperBox: React.FC<Props> = ({
+  classes
+}) => {
+  const { formatMessage } = useIntl();
   const theme = useTheme()
-  const [value, setValue] = React.useState(0)
+  const [value, setValue] = React.useState<React.ReactText>()
+  const [dense, setDense] = React.useState(false)
+  const [secondary, setSecondary] = React.useState(false)
   return (
     <Box component={Paper}>
       <CardHeader
@@ -24,7 +36,44 @@ const PaperBox :React.FC<Props> = ({
           variant='subtitle1'>
         </Typography>
         <Typography>Boxes are useful to apply stylings</Typography>
-        <PriceInput onChange={e => setValue(parseFloat(e.target.value))} value={value}/>
+        <PriceInput onChange={e => setValue(e.target.value)} value={value} />
+        <Box>
+          <Grid container spacing={2}>
+            <Grid item xs={12} md={6}>
+              <Typography variant='h6' className={classes.title} onClick={() => setDense(!dense)}>
+                Text only  {dense ? <ExpandMore /> : <ExpandLess />}
+              </Typography>
+              <List dense={dense}>
+                {generate(
+                  <ListItem button onClick={() => setSecondary(!secondary)}>
+                    <ListItemText
+                      primary={'Single-line item ' + (dense ? '(dense)' : '')}
+                      secondary={secondary ? 'Secondary text' : null}
+                    />
+                  </ListItem>,
+                )}
+              </List>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <Typography variant='h6' className={classes.title} onClick={() => setDense(!dense)}>
+                Icon with text {dense ? <ExpandLess /> : <ExpandMore />}
+              </Typography>
+              <List dense={dense}>
+                {generate(
+                  <ListItem button onClick={() => setSecondary(!secondary)}>
+                    <ListItemIcon>
+                      <FolderIcon />
+                    </ListItemIcon>
+                    <ListItemText
+                      primary={'Single-line item ' + (dense ? '(dense)' : '')}
+                      secondary={secondary ? 'Secondary text' : null}
+                    />
+                  </ListItem>,
+                )}
+              </List>
+            </Grid>
+          </Grid>
+        </Box>
       </CardContent>
     </Box>
   );
--- showcase/src/Component/Typo.tsx
+++ showcase/src/Component/Typo.tsx
@@ -1,16 +1,34 @@
 import React from 'react';
-import { Box, CardContent, CardHeader, Paper, Typography, Link } from '@material-ui/core';
+import { Box, CardContent, CardHeader, Paper, Typography, Link, TypographyVariant } from '@material-ui/core';
 import { withStyles, Theme, WithStyles } from '@material-ui/core/styles';
 import { useIntl } from 'react-intl';
 type Props = {
 } & WithStyles
 const style = (theme: Theme) => ({})
+
+const variants: TypographyVariant[] = [
+  'h1',
+  'h2',
+  'h3',
+  'h4',
+  'h5',
+  'h6',
+  'subtitle1',
+  'subtitle2',
+  'body1',
+  'body2',
+  'button',
+  'caption',
+  'overline',
+]
 const Typo: React.FC<Props> = ({
   classes
 }) => {
   return (
     <Box component={Paper} flexGrow={1}>
-      <CardHeader title='Typographies (Component: CardHeader {Prop: title})' subheader={'(h1-6, body1-2, subtitle1-2, caption)  (Component: CardHeader {Prop: subheader}'} />
+      <CardHeader 
+        title='Typographies (Component: CardHeader {Prop: title})' 
+        subheader={'(h1-6, body1-2, subtitle1-2, caption)  (Component: CardHeader {Prop: subheader}'} />
       <CardContent>
         <Typography
           variant='subtitle1'
@@ -23,52 +41,12 @@ const Typo: React.FC<Props> = ({
             <>
               property of the Typography .
             </>
+            [<Link href='https://material.io/design/typography/the-type-system.html#type-scale' >Specs</Link>]
           </>
         </Typography >
-        <Typography
-          component='li'
-          variant='h1'>variant='h1'</Typography >
-        <Typography
-          component='li'
-          variant='h2'>variant='h2'</Typography >
-        <Typography
-          component='li'
-          variant='h3'>variant='h3'</Typography >
-        <Typography
-          component='li'
-          variant='h4'>variant='h4'</Typography >
-        <Typography
-          component='li'
-          variant='h5'>variant='h5'</Typography >
-        <Typography
-          component='li'
-          variant='h6'>variant='h6'</Typography >
-        <Typography
-          component='li'
-          variant='subtitle1'>variant='subtitle1'</Typography >
-        <Typography
-          component='li'
-          variant='subtitle2'>variant='subtitle2'</Typography >
-        <Typography
-          component='li'
-          variant='body1'>variant='body1'</Typography >
-        <Typography
-          component='li'
-          variant='body2'>variant='body2'</Typography >
-        <Typography
-          component='li'
-          variant='button'>variant='button'</Typography >
-        <Typography
-          component='li'
-          variant='caption'>variant='caption'</Typography >
-        <Typography
-          component='li'
-          variant='srOnly'>variant='srOnly'</Typography >
-        <Typography
-          component='li'
-          variant='overline'>variant='overline'</Typography >
+        {variants.map((variant, index) => <Typography key={variant} variant={variant as TypographyVariant}>variant='{variant}'</Typography>)}
         <Typography > custom variants will be available in material-ui version 5.X (<Link target='_blank'
-          href="https://github.com/mui-org/material-ui/issues/22257">ISSUE</Link>)</Typography >
+          href='https://github.com/mui-org/material-ui/issues/22257'>ISSUE</Link>)</Typography >
         <Typography > We should define custom Typography  styles by defining new variant types.</Typography >
       </CardContent>
       <>text without typography  in Paper and outside CardContent</>
