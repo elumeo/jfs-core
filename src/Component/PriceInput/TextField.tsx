7d0c43d21c06f18881e88df1a8a5bf4593c54b56
pengbang <yeongi41@googlemail.com>
2021-06-07 13:32:05 +0200

    TECH-483: add augmentations. formatting priceinput

--- src/Component/PriceInput/TextField.tsx
+++ src/Component/PriceInput/TextField.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { default as MUITextField, TextFieldProps } from '@material-ui/core/TextField';
-import { useIntl } from 'react-intl';
 import useCurrency from 'Effect/useCurrency';
 import { Currency } from 'Utilities/Format';
 type Props = {
@@ -9,30 +8,44 @@ type Props = {
 const TextField: React.FC<Props> = ({
     currency = useCurrency(),
     value = 0.00,
-    ...rest
+    ...props
 }) => {
     const { getCurrency } = Currency;
     const inputRef = React.useRef<HTMLInputElement>()
     const [sanitized, setSanitized] = React.useState(`${value}`)
-    const [_focused, setFocused] = React.useState(rest.focused)
-    const sanitize = React.useCallback(() => {
-        setSanitized(parseFloat(sanitized.replace(Currency.replaceAllNonNumericOrSeperatorRegex, '')).toFixed(2))
-    }, [setSanitized, sanitized])
-    const display = React.useMemo(() =>
-        getCurrency(currency, parseFloat(sanitized), true)
-        , [sanitized, currency])
-    const _onBlur = React.useCallback((e) => {
-        setFocused(false);
-        sanitize()
-        rest?.onBlur?.(e)
-    }, [setFocused, setSanitized, sanitized,rest?.onBlur])
-    const _onFocus = React.useCallback((e) => {
-        setFocused(true);
-        rest?.onFocus?.(e);
-    }, [setFocused,rest?.onFocus])
-    React.useEffect(() => {
-        setSanitized(`${value}`)
-    }, [value])
+    const [_focused, setFocused] = React.useState(props?.focused)
+    const sanitize = React.useCallback(
+        () => {
+            setSanitized(parseFloat(sanitized.replace(Currency.replaceAllNonNumericOrSeperatorRegex, '')).toFixed(2))
+        },
+        [setSanitized, sanitized]
+    )
+    const display = React.useMemo(
+        () =>
+            getCurrency(currency, parseFloat(sanitized), true)
+        , [sanitized, currency]
+    )
+    const _onBlur = React.useCallback(
+        (e) => {
+            setFocused(false);
+            sanitize()
+            props?.onBlur?.(e)
+        },
+        [setFocused, setSanitized, sanitized, props?.onBlur]
+    )
+    const _onFocus = React.useCallback(
+        (e) => {
+            setFocused(true);
+            props?.onFocus?.(e);
+        },
+        [setFocused, props?.onFocus]
+    )
+    React.useEffect(
+        () => {
+            setSanitized(`${value}`)
+        },
+        [value]
+    )
 
     return (
         <MUITextField
@@ -40,7 +53,7 @@ const TextField: React.FC<Props> = ({
             value={_focused ? sanitized : display}
             onBlur={_onBlur}
             onFocus={_onFocus}
-            {...rest} />
+            {...props} />
     );
 };
 export default TextField;
