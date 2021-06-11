2e1e1d1622cce52d4dbc95cae9e94191b5917b1d
Pascal Brauns <pascal.brauns@juwelo.de>
2021-06-11 08:44:30 +0200

    TECH-483: Fixed casing error

--- build/Component/Websocket/Status/Indicator.d.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import * as Type from '../../../Types/Configuration';
-export declare type Props = {
-    client: Type.WebSocketClient;
-};
-declare const Indicator: React.FC<Props>;
-export default Indicator;
