07f335b9cb07d5603602887647154c08436a499b
Pascal Brauns <pascal.brauns@juwelo.de>
2021-06-28 11:16:10 +0200

    TECH-483: Rebuilded scripts directory

--- scripts/src/Library/JFS/Project/Api/index.ts
+++
@@ -1,7 +0,0 @@
-export * as Check from './Check';
-export * as Description from './Description';
-export * as Generator from './Generator';
-export * as Adapter from './Adapter';
-export * as Code from './Code';
-export * as Config from './Config';
-export * as Render from './Render';
