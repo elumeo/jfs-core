e63c35dfdd589d979e2ed34f7ec2b628e3b0a180
Michael Berkhahn <michaelberkhahn@michaelberkhahn.com>
2021-06-30 09:12:36 +0200

    TECH-483: added msisdn table cell

---
+++ build/Component/Table/TableCell/TableCellMsisdn.d.ts
@@ -0,0 +1,8 @@
+import React from 'react';
+import { TableCellProps } from 'react-virtualized';
+export declare type TableCellMsisdnProps = {
+    cellProps: TableCellProps;
+    rowHeight: number;
+};
+declare const _default: React.MemoExoticComponent<({ cellProps, rowHeight }: TableCellMsisdnProps) => JSX.Element>;
+export default _default;
