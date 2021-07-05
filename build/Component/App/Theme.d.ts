ec3230b29900b341b77755c01e3dc0faf1ed6aa5
Michael Berkhahn <michaelberkhahn@michaelberkhahn.com>
2021-07-05 16:48:47 +0200

    TECH-483: made colors more complete

--- build/Component/App/Theme.d.ts
+++ build/Component/App/Theme.d.ts
@@ -1,2 +1,3 @@
+export declare const JuweloTheme: import("@material-ui/core/styles").Theme;
 declare const Theme: () => import("@material-ui/core/styles").Theme;
 export default Theme;
--- src/Component/App/Theme.tsx
+++ src/Component/App/Theme.tsx
@@ -2,52 +2,48 @@ import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
 import * as Color from 'Constant/Color';
 import { grey } from '@material-ui/core/colors';
 
-const Theme = () => {
-  let theme = createMuiTheme({
-    overrides: {
-      MuiCssBaseline: {
-        '@global': {
-          html: {
-            // fontSize: '87.5%'
-          },
-          body: {
-            margin: 0,
-            padding: 0,
-            fontFamily: 'Roboto',
-          }
+export const JuweloTheme = responsiveFontSizes(createMuiTheme({
+  overrides: {
+    MuiCssBaseline: {
+      '@global': {
+        body: {
+          margin: 0,
+          padding: 0,
+          fontFamily: 'Roboto',
         }
       }
-    },
-    typography: {
-      fontSize: 13,
-    },
-    palette: {
-      type: 'light',
-      primary: Color.rubin,
-      secondary: Color.apatith,
-      text: {
-        primary: grey[900],
-        secondary: grey[700],
-        disabled: grey[500],
-        hint: grey[300],
-      },
-      success: Color.success,
-      warning: Color.warning,
-      error: Color.error,
-      rubin: Color.rubin,
-      rodolith: Color.rodolith,
-      topas: Color.topas,
-      apatith: Color.apatith,
-      peridot: Color.peridot,
-      citrin: Color.citrin,
-      quarz: Color.quarz,
-      background: {
-        default: Color.quarz.main,
-      }
     }
-  });
+  },
+  typography: {
+    fontSize: 13,
+  },
+  palette: {
+    type: 'light',
+    primary: Color.rubin,
+    secondary: Color.apatith,
+    text: {
+      primary: grey[900],
+      secondary: grey[700],
+      disabled: grey[500],
+      hint: grey[300],
+    },
+    success: Color.success,
+    warning: Color.warning,
+    error: Color.error,
+    rubin: Color.rubin,
+    rodolith: Color.rodolith,
+    topas: Color.topas,
+    apatith: Color.apatith,
+    peridot: Color.peridot,
+    citrin: Color.citrin,
+    quarz: Color.quarz,
+    background: {
+      default: Color.quarz.main,
+    }
+  }
+}));
 
-  theme = responsiveFontSizes(theme)
-  return theme
+const Theme = () => {
+  return JuweloTheme;
 };
 export default Theme;
