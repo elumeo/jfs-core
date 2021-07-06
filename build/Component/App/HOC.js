ad6ee5dbe2f324a108dd85cce2da587d271c325f
Michael Berkhahn <michaelberkhahn@michaelberkhahn.com>
2021-07-06 08:23:24 +0200

    TECH-483: made colors a property of theme palette

--- build/Component/App/HOC.js
+++ build/Component/App/HOC.js
@@ -2,19 +2,18 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import { ThemeProvider } from '@material-ui/core/styles';
-import Theme from './Theme';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import MomentUtils from '@date-io/moment';
-import WebSocketConnection from '../WebSocket/WebSocketConnection';
-import Head from './Head';
 import { ConnectedRouter } from 'connected-react-router';
+import JuweloTheme from './Theme';
+import WebSocketConnection from '../WebSocket/WebSocketConnection';
+import HtmlHead from './HtmlHead';
 import { history } from '../../Store/Middleware';
 const HOC = ({ title, store, children }) => {
-    const theme = Theme();
     return (React.createElement(Provider, { store: store },
         React.createElement(MuiPickersUtilsProvider, { utils: MomentUtils },
-            React.createElement(ThemeProvider, { theme: theme },
-                React.createElement(Head, { title: title }),
+            React.createElement(ThemeProvider, { theme: JuweloTheme },
+                React.createElement(HtmlHead, { title: title }),
                 React.createElement(CssBaseline, null),
                 React.createElement(WebSocketConnection, null,
                     React.createElement(ConnectedRouter, { history: history }, children))))));
