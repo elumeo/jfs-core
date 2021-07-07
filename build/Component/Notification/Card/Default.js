78e9068950f93bff45f3c349c75ca578117bd8f7
Michael Berkhahn <michaelberkhahn@michaelberkhahn.com>
2021-07-07 11:27:24 +0200

    TECH-483: removed span as component from notification card

--- build/Component/Notification/Card/Default.js
+++ build/Component/Notification/Card/Default.js
@@ -10,9 +10,9 @@ const DefaultNotificationCard = ({ notification: { title, subtitle, content, act
     const { formatMessage } = useIntl();
     return (React.createElement(React.Fragment, null,
         React.createElement(Box, { component: CardContent, color: 'inherit' },
-            React.createElement(Typography, { variant: 'subtitle1', component: 'span' }, isTranslationId ? formatMessage({ id: title }) : title),
-            React.createElement(Typography, { variant: 'subtitle2', component: 'span' }, isTranslationId ? formatMessage({ id: subtitle }) : subtitle),
-            React.createElement(Typography, { variant: 'body1', component: 'span' }, isTranslationId ? formatMessage({ id: title }) : content)),
-        !temporary && action && (React.createElement(CardActions, null, action(snackbar, id, temporary)))));
+            React.createElement(Typography, { variant: 'subtitle1' }, isTranslationId ? formatMessage({ id: title }) : title),
+            React.createElement(Typography, { variant: 'subtitle2' }, isTranslationId ? formatMessage({ id: subtitle }) : subtitle),
+            React.createElement(Typography, { variant: 'body1' }, isTranslationId ? formatMessage({ id: content }) : content)),
+        !temporary && action && React.createElement(CardActions, null, action(snackbar, id, temporary))));
 };
 export default DefaultNotificationCard;
--- src/Component/Notification/Card/Default.tsx
+++ src/Component/Notification/Card/Default.tsx
@@ -6,44 +6,33 @@ import CardActions from '@material-ui/core/CardActions';
 import { useSnackbar } from 'notistack';
 import * as Type from 'Types/Notification';
 import { useIntl } from 'react-intl'
+
 export type Props = {
   notification: Type.Notification;
   temporary?: boolean;
 };
 
 const DefaultNotificationCard: React.FC<Props> = ({
-  notification: {
-    title, subtitle, content, action, id, isTranslationId = false
-  },
-  temporary
-}) => {
+                                                    notification: {
+                                                      title,
+                                                      subtitle,
+                                                      content,
+                                                      action,
+                                                      id,
+                                                      isTranslationId = false
+                                                    },
+                                                    temporary
+                                                  }) => {
   const snackbar = useSnackbar();
-  const { formatMessage } = useIntl()
-  return (
-    <>
-      <Box component={CardContent} color='inherit'>
-        <Typography
-          variant='subtitle1'
-          component='span'>
-          {isTranslationId ? formatMessage({ id: (title as string) }) : title}
-        </Typography>
-        <Typography
-          variant='subtitle2'
-          component='span'>
-          {isTranslationId ? formatMessage({ id: (subtitle as string) }) : subtitle}
-        </Typography>
-        <Typography
-          variant='body1'
-          component='span'>
-          {isTranslationId ? formatMessage({ id: (title as string) }) : content}
-        </Typography>
-      </Box>
-      {!temporary && action && (
-        <CardActions>
-          {action(snackbar, id, temporary)}
-        </CardActions>
-      )}
-    </>
-  )
+  const {formatMessage} = useIntl()
+  return (<>
+    <Box component={CardContent} color='inherit'>
+      <Typography variant='subtitle1'>{isTranslationId ? formatMessage({id: (title as string)}) : title}</Typography>
+      <Typography variant='subtitle2'>{isTranslationId ? formatMessage({id: (subtitle as string)}) : subtitle}</Typography>
+      <Typography variant='body1'>{isTranslationId ? formatMessage({id: (content as string)}) : content}</Typography>
+    </Box>
+    {!temporary && action && <CardActions>{action(snackbar, id, temporary)}</CardActions>}
+  </>)
 }
+
 export default DefaultNotificationCard
