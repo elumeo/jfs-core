cc3a8c60919399f4c0d2a9ff3c6ff5dc4a5e8fc0
Michael Berkhahn <michaelberkhahn@michaelberkhahn.com>
2021-09-30 10:12:24 +0200

    TECH-483: added more table cells

--- build/Component/Table/TableCell/index.d.ts
+++ build/Component/Table/TableCell/index.d.ts
@@ -1,2 +1,5 @@
 export { default as TableCellDefault } from './TableCellDefault';
 export { default as TableCellLoading } from './TableCellLoading';
+export { default as TableCellMsisdn } from './TableCellMsisdn';
+export { default as TableCellDateTime } from './TableCellDateTime';
+export { default as TableCellDateTimeRange } from './TableCellDateTimeRange';
--- build/Component/Table/TableCell/index.js
+++ build/Component/Table/TableCell/index.js
@@ -3,8 +3,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.TableCellLoading = exports.TableCellDefault = void 0;
+exports.TableCellDateTimeRange = exports.TableCellDateTime = exports.TableCellMsisdn = exports.TableCellLoading = exports.TableCellDefault = void 0;
 var TableCellDefault_1 = require("./TableCellDefault");
 Object.defineProperty(exports, "TableCellDefault", { enumerable: true, get: function () { return __importDefault(TableCellDefault_1).default; } });
 var TableCellLoading_1 = require("./TableCellLoading");
 Object.defineProperty(exports, "TableCellLoading", { enumerable: true, get: function () { return __importDefault(TableCellLoading_1).default; } });
+var TableCellMsisdn_1 = require("./TableCellMsisdn");
+Object.defineProperty(exports, "TableCellMsisdn", { enumerable: true, get: function () { return __importDefault(TableCellMsisdn_1).default; } });
+var TableCellDateTime_1 = require("./TableCellDateTime");
+Object.defineProperty(exports, "TableCellDateTime", { enumerable: true, get: function () { return __importDefault(TableCellDateTime_1).default; } });
+var TableCellDateTimeRange_1 = require("./TableCellDateTimeRange");
+Object.defineProperty(exports, "TableCellDateTimeRange", { enumerable: true, get: function () { return __importDefault(TableCellDateTimeRange_1).default; } });
