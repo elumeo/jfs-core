61967ea5879fe35d530f428933890aa9f89313a2
Michael Berkhahn <michaelberkhahn@michaelberkhahn.com>
2021-11-23 18:23:04 +0100

    TECH-483: typing fixes

--- build/Component/Table/TableCell/TableCellDateTime.d.ts
+++ build/Component/Table/TableCell/TableCellDateTime.d.ts
@@ -1,6 +1,6 @@
 import React, { ReactNode } from 'react';
 export declare type TableCellDateTimeProps = {
-    cellData: string;
+    cellData: Date | string;
     noValueElement?: ReactNode;
 };
 declare const _default: React.MemoExoticComponent<({ cellData, noValueElement }: TableCellDateTimeProps) => JSX.Element>;
