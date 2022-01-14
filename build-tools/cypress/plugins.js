bb6abd0124936b431eca13a1620817b5e4a2b218
robertNeuner <robert.neuner@juwelo.de>
2022-01-14 13:50:56 +0100

    TECH-483: Fixed - by using module.exports...

--- build-tools/cypress/plugins.js
+++ build-tools/cypress/plugins.js
@@ -2,8 +2,8 @@ const injectDevServer = require("@cypress/react/plugins/react-scripts")
 const {addMatchImageSnapshotPlugin} = require('cypress-image-snapshot/plugin')
 
 // noinspection JSUnusedLocalSymbols
-function setupJfsCoreCypressPlugins(on, config) {
-  injectDevServer(on, config, {webpackConfigPath: '@elumeo/jfs-core/settings/webpack.development'})
+module.exports.setupJfsCoreCypressPlugins = (on, config) => {
+  injectDevServer(on, config, {webpackConfigPath: '@elumeo/jfs-core/build-tools/webpack/dev-server'})
   addMatchImageSnapshotPlugin(on, config)
   on('before:browser:launch', (browser = {}, launchOptions) => {
     if (browser.family === 'chromium' && browser.name !== 'electron') {
