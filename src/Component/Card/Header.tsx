9912a10c36e9a920cfd14a7ed176526a22e36973
michaelberkhahn <michaelberkhahn@michaelberkhahn.com>
2023-04-14 16:57:34 +0200

    STORY-6780: fixed wrong position of linear loading indicator in card header

--- src/Component/Card/Header.tsx
+++ src/Component/Card/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { CardHeader, LinearProgress, PropTypes, Stack, SxProps, Typography } from '@mui/material';
-import { IconButtonProps } from '@mui/material/IconButton';
+import {CardHeader, LinearProgress, PropTypes, Stack, SxProps, Typography} from '@mui/material';
+import {IconButtonProps} from '@mui/material/IconButton';
 import RefreshButton from 'Component/Button/RefreshButton';
 
 export type AppCardHeaderBaseProps = {
@@ -17,47 +17,28 @@ const hiddenStyle: SxProps = {
   visibility: 'hidden',
 }
 const Header: React.FC<AppCardHeaderBaseProps> = ({
-  isLoading = false,
-  title,
-  subtitle = null,
-  titleIcon = null,
-  onRefresh = null,
-  refreshButtonColor = 'secondary',
-  refreshButtonSize = 'small',
-  action = null
-}) => <>
-    <CardHeader
-      subheader={subtitle}
-      action={action}
-      title={
-        <>
-          <Stack
-            direction={'row'}
-            spacing={1}
-            alignItems={'center'}
-          >
-            {
-              titleIcon
-            }
-            {typeof title === 'string' ? <Typography variant='h5'>{title}</Typography> : title}
-            {
-              onRefresh !== null
-              &&
-              <RefreshButton
-                color={refreshButtonColor}
-                size={refreshButtonSize}
-                inProgress={isLoading}
-                onClick={onRefresh} />
-            }
-          </Stack>
-        </>
-      }
-    />
-    <LinearProgress color='secondary' sx={
-      isLoading
-        ? {}
-        : hiddenStyle
-    } />
-  </>;
+                                                    isLoading = false,
+                                                    title,
+                                                    subtitle = null,
+                                                    titleIcon = null,
+                                                    onRefresh = null,
+                                                    refreshButtonColor = 'secondary',
+                                                    refreshButtonSize = 'small',
+                                                    action = null
+                                                  }) => <>
+  <LinearProgress color='secondary' sx={isLoading ? {} : hiddenStyle}/>
+  <CardHeader
+    subheader={subtitle}
+    action={action}
+    disableTypography
+    title={<>
+      <Stack direction={'row'} spacing={1} alignItems={'center'}>
+        {titleIcon}
+        {typeof title === 'string' ? <Typography variant='h5'>{title}</Typography> : title}
+        {onRefresh !== null && <RefreshButton color={refreshButtonColor} size={refreshButtonSize} inProgress={isLoading} onClick={onRefresh}/>}
+      </Stack>
+    </>}
+  />
+</>;
 
 export default Header
