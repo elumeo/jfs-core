085e439ef853ec6ed857b413286dd57b24a52aed
michaelberkhahn <michaelberkhahn@michaelberkhahn.com>
2023-04-18 14:30:05 +0200

    STORY-6780: fixed wrong import of skeleton

--- build/Component/Table/Cell/Loading.js
+++ build/Component/Table/Cell/Loading.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+var material_1 = require("@mui/material");
 var react_1 = __importDefault(require("react"));
-var lab_1 = require("@mui/lab");
-var Loading = function () { return react_1.default.createElement(lab_1.Skeleton, { variant: 'text', width: '100%', height: '100%', animation: 'wave' }); };
+var Loading = function () { return react_1.default.createElement(material_1.Skeleton, { variant: 'text', width: '100%', height: '100%', animation: 'wave' }); };
 exports.default = Loading;
--- src/Component/Table/Cell/Loading.tsx
+++ src/Component/Table/Cell/Loading.tsx
@@ -1,5 +1,5 @@
+import { Skeleton } from '@mui/material';
 import React from 'react';
-import { Skeleton } from '@mui/lab';
 
 const Loading: React.FC = () => <Skeleton variant='text' width={'100%'} height={'100%'} animation={'wave'} />;
 
