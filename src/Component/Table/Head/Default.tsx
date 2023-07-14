2a5497aea61c1e4d0069859416f7d05ce5c3332d
Michael Berkhahn <contact@michaelberkhahn.com>
2023-07-14 15:31:20 +0200

    table head accepts sx props now without loosing width styling

--- src/Component/Table/Head/Default.tsx
+++ src/Component/Table/Head/Default.tsx
@@ -1,8 +1,8 @@
-import React, { useMemo } from 'react';
-import { TableCell, TableSortLabel, SortDirection, SxProps, TableCellProps, Typography, TableSortLabelProps } from '@mui/material';
-import { visuallyHiddenStyle } from 'Component/Table/VirtualizedTable';
-import { grey } from '@mui/material/colors';
-import { apatith } from 'Constant/Color';
+import React, {useMemo} from 'react';
+import {TableCell, TableSortLabel, SortDirection, SxProps, TableCellProps, Typography, TableSortLabelProps} from '@mui/material';
+import {visuallyHiddenStyle} from 'Component/Table/VirtualizedTable';
+import {grey} from '@mui/material/colors';
+import {apatith} from 'Constant/Color';
 import definition from 'Component/App/Stateless/Style/Theme/Definition';
 
 export const sortingStyles: SxProps = {
@@ -22,7 +22,7 @@ export type Props = Omit<TableCellProps, 'onClick'> & {
   onClick?: (sortBy: string, sortDirection: SortDirection) => void
 };
 
-const Default: React.FC<Props> = ({ height = 48, isNumeric = false, disableSort = false, sortBy, sortDirection, onClick, label, dataKey, width, ...rest }) => {
+const Default: React.FC<Props> = ({height = 48, isNumeric = false, disableSort = false, sortBy, sortDirection, onClick, label, dataKey, width, sx, ...rest}) => {
   const isActiveSort = sortBy === dataKey
   const color = isActiveSort || !disableSort
     ? apatith.main
@@ -32,23 +32,24 @@ const Default: React.FC<Props> = ({ height = 48, isNumeric = false, disableSort 
       ...(isActiveSort ? sortingStyles : {}),
       height: height,
       maxWidth: '100%',
-      width: width
+      width: width,
+      ...sx
     }
-  ), [sortBy, isActiveSort, height, width]);
+  ), [sortBy, isActiveSort, height, width, sx]);
 
   const sort: TableCellProps['onClick'] = () => disableSort || (onClick !== undefined && onClick(dataKey, isActiveSort ? (sortDirection === 'asc' ? 'desc' : 'asc') : 'asc'));
   return <TableCell
-    variant='head'
+    variant="head"
     sx={styles}
     align={rest.align ? rest.align : isNumeric ? 'right' : 'left'}
     onClick={sort}
     {...rest}
   >
-    {disableSort && <Typography fontWeight={600} variant='subtitle1'>{label}</Typography>}
-    {!disableSort && <TableSortLabel active={isActiveSort} direction={sortDirection as TableSortLabelProps['direction']} sx={{ color }}>
-      <Typography fontWeight={600} lineHeight={1} variant='subtitle1' color={color}>{label}</Typography>
+    {disableSort && <Typography fontWeight={600} variant="subtitle1">{label}</Typography>}
+    {!disableSort && <TableSortLabel active={isActiveSort} direction={sortDirection as TableSortLabelProps['direction']} sx={{color}}>
+      <Typography fontWeight={600} lineHeight={1} variant="subtitle1" color={color}>{label}</Typography>
       {isActiveSort
-        ? <Typography component='span' sx={visuallyHiddenStyle}>
+        ? <Typography component="span" sx={visuallyHiddenStyle}>
           {`${sortDirection}`.toLowerCase() === 'desc' ? 'sorted descending' : 'sorted ascending'}
         </Typography>
         : null
