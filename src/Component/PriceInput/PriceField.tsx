1ba5f1222d24a19f7b3373009df839f394a7d239
theRealPengBang <yeongi41@googlemail.com>
2023-12-05 20:29:18 +0100

    price-field: rework

--- src/Component/PriceInput/PriceField.tsx
+++ src/Component/PriceInput/PriceField.tsx
@@ -1,161 +1,113 @@
 import React from 'react';
-import { InputAdornment } from '@mui/material';
+import { InputAdornment, TextField } from '@mui/material';
 import TextFieldClearButton, { type Props as TextFieldProps } from 'Component/TextField'
-import useCurrency from 'Effect/useCurrency';
 import usePriceFieldAdornment from 'Effect/usePriceFieldAdornment';
 import { Currency } from 'Utilities/Format';
-import { createIntl, createIntlCache } from 'react-intl';
+import { createIntl, createIntlCache, useIntl } from 'react-intl';
+import { mapLanguageToLocale } from 'Utilities/Format/Locale';
+import { LANGUAGE } from 'Types/Language';
+import { satisfies } from 'semver';
 
 type Props = {
+  language?: LANGUAGE;
   currency?: string;
   selectOnFocus?: boolean;
-  value: number;
-  min?: number;
-  max?: number;
+  valueInCent?: number;
+  min?: number; //@INFO: min amount in cent
+  max?: number; //@INFO: max amount in cent
   showDecimals?: boolean
-} & TextFieldProps
+  textFieldProps?: TextFieldProps,
+  setValue?: (value: number) => void
+}
+const sanitize = (value: string, decimalSeparator: string): string => {
+  let returnValue = value;
+  if (!returnValue || returnValue == 'null') { return null; }
+
+  // @INFO: if user only inputs integers, the assumptions is, that the user wants to input euros instead of cents
+
+  // if (!returnValue.includes(decimalSeparator) || returnValue[returnValue.length - 1] === decimalSeparator) {
+  //   if (!returnValue.includes(decimalSeparator)) {
+  //     returnValue = `${value}${decimalSeparator}0`
+  //   } else {
+  //     returnValue = `${value}0`
+  //   }
+  // }
+
+  // @INFO: bspw: 12.1, 23,5 => 12.10, 23,50
+  if (returnValue?.[returnValue.length - 2] === decimalSeparator) {
+    returnValue = `${returnValue}0`
+  }
+  const sanitizedString = returnValue.replace(/[^\d-]|(?<=\d)-/g, '');
+  return sanitizedString;
+}
+const checkForGroupingNumberFormat = (groupingSeparator: string, decimalSeparator: string) => new RegExp(
+  `^-?((\\d{1,3}(${groupingSeparator == '.' ? `\\.` : groupingSeparator}\\d{3})*(${decimalSeparator == '.' ? `\\.` : decimalSeparator}\\d{1,2})?)|\\d+(${decimalSeparator == '.' ? `\\.` : decimalSeparator}?\\d{1,2})?)$`,
+  'g'
+)
+const checkForNonGroupingNumberFormat = (decimalSeparator: string) =>
+  new RegExp(
+    `^-?(\\d)*(\\${decimalSeparator == '.' ? `\\.` : decimalSeparator}\\d{1,2})?$`,
+    'g'
+  )
+const isValid = (value: string | number, groupingSeparator: string, decimalSeparator: string) => {
+
+  const isValidWithGrouping = `${value}`.match(checkForGroupingNumberFormat(groupingSeparator, decimalSeparator))?.length > 0
+  const isValidWithoutGrouping = `${value}`.match(checkForNonGroupingNumberFormat(decimalSeparator))?.length > 0
+  return isValidWithGrouping || isValidWithoutGrouping
+}
 
 const PriceField: React.FC<Props> = ({
-                                       onChange,
-                                       currency = 'eur',
-                                       value = 0.0,
-                                       selectOnFocus = true,
-                                       showDecimals = true,
-                                       min,
-                                       max,
-                                       ...props
-                                     }) => {
-  // const { formatNumberToParts, formatNumber } = useIntl();
-  const cache = createIntlCache();
-  const { formatNumberToParts, formatNumber } = createIntl({
-    locale: 'de-DE',
-    // locale: 'en-US',
-    messages: {},
-  }, cache);
-  const currencyDecimalSeparator = formatNumberToParts(0.1)[1].value;
-  const currencyGroupSeparator = formatNumberToParts(1000)[1].value;
-  console.log('locale currency separators', currencyDecimalSeparator, currencyGroupSeparator);
-  const configCurrency = useCurrency();
-  const ref = React.useRef(null)
-  const finalCurrency = currency ?? configCurrency;
-  const [rawValue, setRawValue] = React.useState<number>(null);
-  const [displayValue, setDisplayValue] = React.useState<string>(null);
-  const [isNegativeValue, setIsNegativeValue] = React.useState(false);
-  // const display = rawValue !== null ? formatNumber(rawValue, { style: 'decimal', maximumFractionDigits: 2, minimumFractionDigits: 2 }) : '';
-  // const display = value === ''
-  //   ? ''
-  //   : Currency.getCurrency(
-  //     finalCurrency,
-  //     isNaN(Number(value))
-  //       ? 0
-  //       : Number(value),
-  //     true,
-  //     false,
-  //     showDecimals
-  //   )
-  const [adornmentType, adornmentPosition, styles] = usePriceFieldAdornment(finalCurrency);
-  const [_focused, setFocused] = React.useState(props.focused);
-  React.useEffect(() => {
-    if (selectOnFocus && _focused) {
-      ref.current.select();
-    }
-  }, [_focused, selectOnFocus]);
-  // const _onFocus: TextFieldProps['onFocus'] = React.useCallback(event => {
-  //   setFocused(true);
-  //   props?.onFocus?.(event);
-  // }, [props?.onFocus, setFocused, selectOnFocus]);
+  currency = 'eur',
+  valueInCent,
+  language = LANGUAGE.GERMAN,
+  selectOnFocus = true,
+  showDecimals = false,
+  min = -Infinity,
+  max = Infinity,
+  textFieldProps: { required, ...textFieldProps } = { required: false },
+  setValue,
+}) => {
 
-  // const _onBlur: TextFieldProps['onBlur'] = React.useCallback(event => {
-  //   props?.onBlur?.(event);
-  //   setFocused(false);
-  //   if (isNaN(parseFloat(value as string))) {
-  //     props.onChange({ ...event, target: { ...event.target, value: null } } as React.ChangeEvent<HTMLInputElement>)
-  //   }
-  // }, [setFocused, props?.onBlur, value]);
+  const [position, at] = usePriceFieldAdornment(currency)
+  const { formatNumber } = useIntl()
+  const locale = mapLanguageToLocale(language);
+  const [localValue, setLocalValue] = React.useState((valueInCent ?? '').toLocaleString(locale, { useGrouping: false }));
+  const decimalSeparator = (1.1).toLocaleString(locale).substring(1, 2);
+  const groupingSeparator = (1000).toLocaleString(locale).substring(1, 2);
+  const sanitizedLocalValue = sanitize(localValue, decimalSeparator);
+  const outOfRange = (!!valueInCent)
+    && ((min !== -Infinity && valueInCent < min) || (max !== Infinity && valueInCent > max))
+  const isLocalValueValid = isValid(localValue, groupingSeparator, decimalSeparator);
+  const hasErrors = !isLocalValueValid || outOfRange;
+  console.log({ decimalSeparator, groupingSeparator, valueInCent, localValue, sanitizedLocalValue })
 
-  const _onChange: TextFieldProps['onChange'] = React.useCallback((event) => {
-    const caret = event.target.selectionStart;
-    const target = event.target;
-    const isNegativeValue = event.target.value.startsWith('-');
-    const numberOfGroupSeparatorsBeforeChange = formatNumberToParts(rawValue).filter(({ type }) => type === 'group').length;
-    // @ts-ignore
-    if(event.nativeEvent.data === currencyDecimalSeparator && event.target.value.indexOf(currencyDecimalSeparator) !== -1) {
-      window.requestAnimationFrame(() => {
-        console.log({ numberOfGroupSeparatorsBeforeChange, numberOfGroupSeparatorsAfterChange });
-        const newCaretPosition = numberOfGroupSeparatorsAfterChange > numberOfGroupSeparatorsBeforeChange
-          ? caret + 1
-          : numberOfGroupSeparatorsAfterChange < numberOfGroupSeparatorsBeforeChange
-            ? caret - 1
-            : caret;
-        target.selectionStart = newCaretPosition;
-        target.selectionEnd = newCaretPosition;
-      });
-      return;
-    }
-    const cleanedValue = event.target.value
-      .replace(/[a-zA-Z+-]/g, '')
-      .replace(currencyGroupSeparator, '')
-      .replace(currencyDecimalSeparator+currencyDecimalSeparator, '.')
-      .replace(currencyDecimalSeparator, '.')
-    ;
-    if (cleanedValue === '') {
-      setIsNegativeValue(false);
-      setRawValue(null);
-      setDisplayValue(null);
-      return;
-    } else if (cleanedValue === '-') {
-      setIsNegativeValue(true);
-      setRawValue(null);
-      setDisplayValue('-');
-      return;
-    }
-    const _rawValue = +cleanedValue;
-    const numberOfGroupSeparatorsAfterChange = formatNumberToParts(_rawValue).filter(({ type }) => type === 'group').length;
-    const _displayValue = formatNumber(_rawValue, { style: 'decimal', maximumFractionDigits: 2, minimumFractionDigits: 2 });
-    console.log('values', { ori: event.target.value, cleanedValue, _rawValue, _displayValue });
-    setIsNegativeValue(isNegativeValue);
-    setRawValue(_rawValue);
-    setDisplayValue(_displayValue);
-    window.requestAnimationFrame(() => {
-      console.log({ numberOfGroupSeparatorsBeforeChange, numberOfGroupSeparatorsAfterChange });
-      const newCaretPosition = numberOfGroupSeparatorsAfterChange > numberOfGroupSeparatorsBeforeChange
-        ? caret + 1
-        : numberOfGroupSeparatorsAfterChange < numberOfGroupSeparatorsBeforeChange
-          ? caret - 1
-          : caret;
-      target.selectionStart = newCaretPosition;
-      target.selectionEnd = newCaretPosition;
-    });
-    // onChange?.(
-    //   {
-    //     ...event, target: {
-    //       ...event.target,
-    //       value: `${FormatNumber.parse(_value, min, max)}`
-    //     }
-    //   });
-  }, [onChange, min, max, displayValue]);
-  const _InputProps = React.useMemo(() => (
-      {
-        [adornmentType]: <InputAdornment position={adornmentPosition} sx={styles}>{Currency.getCurrencySign(currency)}</InputAdornment>,
-        ...props?.InputProps,
-      }),
-    [adornmentPosition, adornmentType, currency, props?.InputProps, styles],
-  );
-  console.log('PriceField', { rawValue, displayValue });
-  return (
+  return <>
     <TextFieldClearButton
-      inputRef={ref}
-      value={displayValue}
-      InputProps={_InputProps}
-      onChange={_onChange}
-      onReset={() => console.log('onReset')}
-      onBeforeInputCapture={() => console.log('onBeforeInputCapture')}
-      onChangeCapture={() => console.log('onChangeCapture')}
-      onCompositionEnd={() => console.log('onCompositionEnd')}
-      onCompositionEndCapture={() => console.log('onCompositionEndCapture')}
-      // onFocus={_onFocus}
-      // onBlur={_onBlur}
-      {...props}
-    />)
+      required={required}
+      {...textFieldProps}
+      value={localValue}
+      selectOnFocus={selectOnFocus}
+      error={hasErrors}
+      helperText={hasErrors ? outOfRange ? `min: ${formatNumber(min / 100, { minimumFractionDigits: 2, maximumFractionDigits: 2 })} max: ${formatNumber(max / 100, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}` : 'invalid' : ''}
+      onBlur={() => {
+        setValue(
+          isValid(localValue, groupingSeparator, decimalSeparator)
+            ? sanitize(localValue, decimalSeparator)
+              ? parseInt(sanitize(localValue, decimalSeparator))//Math.min(Math.max(, min), max)
+              : null
+            : null
+        )
+      }}
+      onChange={(e) => {
+        setLocalValue(
+          e.target.value ? (e.target.value).replace(/[^0-9.,-]*/g, '') : null
+        )
+
+      }}
+      InputProps={{
+        [position]: <InputAdornment position={at}>{Currency.getCurrencySign(currency)}</InputAdornment>,
+      }}
+    />
+  </>
 };
 export default PriceField;
