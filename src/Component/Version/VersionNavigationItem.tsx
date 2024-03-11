6cd0da6013d903854e952fc535f3a13bf3bbcb5c
michaelberkhahn <contact@michaelberkhahn.com>
2024-03-11 11:51:26 +0100

    STORY-7279: extended version information about core version number

--- src/Component/Version/VersionNavigationItem.tsx
+++ src/Component/Version/VersionNavigationItem.tsx
@@ -1,15 +1,25 @@
-import {useIntl} from "react-intl";
-import * as Navigation from 'Component/Navigation';
-import React from "react";
-import useCore from "Effect/useCore";
+import { useIntl } from 'react-intl';
+import React from 'react';
+import useCore from 'Effect/useCore';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import InfoIcon from '@mui/icons-material/Info';
+import ListItemText from '@mui/material/ListItemText';
+import packageJson from '../../../package.json';
 
 const VersionNavigationItem: React.FC = () => {
-  const {formatMessage} = useIntl();
-  const {App: {packageJson: {version}}} = useCore()
-  return <Navigation.Item
-    iconName={'info_outlined'}
-    messageString={formatMessage({id: 'app.version'}, {versionNumber: version})}
-  />
+  const { formatMessage } = useIntl();
+  const { App: { packageJson: { version } } } = useCore()
+  return <>
+    <ListItem>
+      <ListItemIcon><InfoIcon color={'info'}/></ListItemIcon>
+      <ListItemText primary={formatMessage({ id: 'app.version' }, { versionNumber: version })}/>
+    </ListItem>
+    <ListItem>
+      <ListItemIcon><InfoIcon color={'info'}/></ListItemIcon>
+      <ListItemText primary={formatMessage({ id: 'core.version' }, { versionNumber: packageJson.version })}/>
+    </ListItem>
+  </>
 }
 
 export default React.memo(VersionNavigationItem);
