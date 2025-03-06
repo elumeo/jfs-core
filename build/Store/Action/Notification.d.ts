b323861262dd9cd81d855ab007da7f69316d9156
ycmj <yeongi41@googlemail.com>
2025-03-06 15:17:29 +0100

    TECH-705: bump version

--- build/Store/Action/Notification.d.ts
+++ build/Store/Action/Notification.d.ts
@@ -1,9 +1,8 @@
 import * as TA from 'typesafe-actions';
 import * as Type from '../../Types/Notification';
-import { AxiosError } from 'axios';
 export declare const addNotification: TA.PayloadActionCreator<"notification/ADD", Type.Notification>;
 export declare const removeNotification: TA.PayloadActionCreator<"notification/REMOVE", string>;
 export declare const removeNotificationGroup: TA.PayloadActionCreator<"notification/REMOVE_GROUP", string>;
 export declare const removeAllNotifications: TA.EmptyActionCreator<"notification/REMOVE_ALL">;
-export declare const addErrorNotification: TA.PayloadActionCreator<"notification/ERROR_ADD", AxiosError<unknown, any>>;
+export declare const addErrorNotification: TA.PayloadActionCreator<"notification/ERROR_ADD", import("axios").AxiosError<unknown, any>>;
 export declare const setIsNotificationHistoryOpen: TA.PayloadActionCreator<"notification/SET_IS_HISTORY_OPEN", boolean>;
