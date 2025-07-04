a0eca3999116fce1d90ba79203de00fff9d5d2fc
user-23 <github@N4soft.de>
2025-07-04 22:16:06 +0200

    TECH-737: Added Spanish and Polish language keys

--- build/Component/App/Stateless/Picker.js
+++ build/Component/App/Stateless/Picker.js
@@ -21,8 +21,10 @@ var locales_1 = require("@mui/x-date-pickers/locales");
 var de_1 = __importDefault(require("date-fns/locale/de"));
 var en_GB_1 = __importDefault(require("date-fns/locale/en-GB"));
 var it_1 = __importDefault(require("date-fns/locale/it"));
-var adapterLocales = { de: de_1.default, en: en_GB_1.default, it: it_1.default };
-var textLocales = { de: locales_1.deDE, en: locales_1.enUS, it: locales_1.itIT };
+var es_1 = __importDefault(require("date-fns/locale/es"));
+var pl_1 = __importDefault(require("date-fns/locale/pl"));
+var adapterLocales = { de: de_1.default, en: en_GB_1.default, it: it_1.default, es: es_1.default, pl: pl_1.default };
+var textLocales = { de: locales_1.deDE, en: locales_1.enUS, it: locales_1.itIT, es: locales_1.esES, pl: locales_1.plPL };
 var Picker = function (_a) {
     var children = _a.children, locale = _a.locale;
     return (0, jsx_runtime_1.jsx)(LocalizationProvider_1.LocalizationProvider, __assign({ dateAdapter: AdapterDateFns_1.AdapterDateFns, adapterLocale: adapterLocales[locale], localeText: textLocales[locale].components.MuiLocalizationProvider.defaultProps.localeText }, { children: children }));
--- build/Types/Language.js
+++ build/Types/Language.js
@@ -6,6 +6,8 @@ var LANGUAGE;
     LANGUAGE["GERMAN"] = "de";
     LANGUAGE["ENGLISH"] = "en";
     LANGUAGE["ITALIAN"] = "it";
+    LANGUAGE["POLISH"] = "pl";
+    LANGUAGE["SPANISH"] = "es";
 })(LANGUAGE = exports.LANGUAGE || (exports.LANGUAGE = {}));
 var DATE_FORMAT;
 (function (DATE_FORMAT) {
@@ -15,4 +17,5 @@ var DATE_FORMAT;
     DATE_FORMAT["FR"] = "dd/MM/yyyy";
     DATE_FORMAT["NL"] = "dd-MM-yyyy";
     DATE_FORMAT["IT"] = "dd/MM/yyyy";
+    DATE_FORMAT["PL"] = "yyyy-MM-dd";
 })(DATE_FORMAT = exports.DATE_FORMAT || (exports.DATE_FORMAT = {}));
